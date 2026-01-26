b3151b2161a2e536e4b8167ccea6925dceeda8d2
MostafaHakim <mdmostafahakim@gmail.com>
2026-01-26 13:55:56 +0600

    new

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = () => {
-  mongoose.connect(process.env.MONGODB_URI).then(() => {
-    console.log("Database connected");
-  });
-};
-
-module.exports = connectDB;
--- backend/router/admin.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const upload = require("../middleware/upload");
-const { createAdmin, loginAdmin } = require("../controller/admin.controller");
-const router = express.Router();
-
-router.post("/", createAdmin);
-router.post("/login", loginAdmin);
-
-module.exports = router;
